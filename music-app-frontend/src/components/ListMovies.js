92cb70969aef3c817952c5761dfce51863f11e79
Hasti Gheibi Dehnashi <hastidehnashi@gmail.com>
2021-03-30 21:03:38 -0700

    Redo yarn build

---
+++ music-app-frontend/src/components/ListMovies.js
@@ -0,0 +1,7 @@
+// import React from "react";
+// //import PropTypes from "prop-types";
+
+// const ListMovies = ({ movies }) => {
+//   //const renderMovies = movies.map((m) => <div>m.title</div>);
+//   return
+// }
---
+++ music-app-frontend/src/pages/Homepage.js
@@ -0,0 +1,19 @@
+import React from "react";
+//import ListMovies from "../components/ListMovies.js";
+
+export default function Homepage() {
+  // const movies = Array.from({ length: 10 }).map((m, i) => ({
+  //   title: `Movie ${i}`,
+  // }));
+  return (
+    <div>
+      <h1>My Front</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, enim
+        fuga possimus, eos aliquid odit obcaecati, quod nisi blanditiis maxime
+        recusandae voluptatibus. Odit sapiente ducimus voluptas natus aliquam
+        iusto nemo. Yeeee!
+      </p>
+    </div>
+  );
+}
