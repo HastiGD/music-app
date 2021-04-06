ccaa29dff3340359318bf00bb89c3e407cf411f1
Hasti Gheibi Dehnashi <hastidehnashi@gmail.com>
2021-04-06 10:53:33 -0700

    Added a function in VideoComponent which capitalizes first letter in cointry

--- music-app-frontend/src/components/VideoComponent.js
+++ music-app-frontend/src/components/VideoComponent.js
@@ -1,10 +1,17 @@
-//import React, { useState, useEffect } from "react";
-
 export default function VideoComponent({ src, country }) {
+  // Capitalizes the first letter of the country name
+  function capCountry() {
+    const words = country.split(" ");
+    let Country = words.map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    });
+    return Country.join(" ");
+  }
+
   console.log("Rendering Video");
   return (
     <div>
-      <h1 className="display-6">Check out this song from {country}</h1>
+      <h1 className="display-6">Check out this song from {capCountry()}</h1>
       <div>
         <iframe
           width="560"
